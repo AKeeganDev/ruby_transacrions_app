5cbe7ca4158f61126d606addda9f2188c593e624
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-08-02 15:25:58 -0400

    setup database and linters

---
+++ db/migrate/20220802191006_create_groups_expenses_join_table.rb
@@ -0,0 +1,8 @@
+class CreateGroupsExpensesJoinTable < ActiveRecord::Migration[7.0]
+  def change
+    create_join_table :groups, :expenses do |t|
+      t.index %i[group_id expense_id]
+      t.index %i[expense_id group_id]
+    end
+  end
+end
