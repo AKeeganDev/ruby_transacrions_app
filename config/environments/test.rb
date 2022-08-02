568afd96da985584172cc3e8c676ad052d90fe56
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-08-02 11:48:09 -0400

    setup gems

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -57,4 +57,10 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.bullet_logger = true
+    Bullet.raise = true # raise an error if an n+1 query occurs
+  end
 end
